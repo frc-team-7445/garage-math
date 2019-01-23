6c726798d85d5ffa0abcffb3b295997629f16a8a
qhdwight <qhdwight@gmail.com>
2019-01-23 08:02:49 -0800

    Initial commit

---
+++ src/matrix.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <array>
+
+namespace garage {
+    namespace math {
+        template<class T=double, int w, int h>
+        struct matrix {
+        private:
+            std::array<std::array<T, w>, h> data;
+        public:
+dawd
+        };
+    }
+}

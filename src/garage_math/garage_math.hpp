8bb435a38471539c555f645927e3c2bfbb952ab6
qhdwight <qhdwight@gmail.com>
2019-01-29 20:30:53 -0800

    Sign for garage robotics

--- src/garage_math/garage_math.hpp
+++ src/garage_math/garage_math.hpp
@@ -1,2 +1,15 @@
+#pragma once
+
 #include <garage_math/matrix.hpp>
 #include <garage_math/vector.hpp>
+
+namespace garage {
+    namespace math {
+        template<typename T>
+        int sign(const T& value) {
+            if (value > T(0)) return 1;
+            else if (value < T(0)) return -1;
+            else return 0;
+        }
+    }
+}

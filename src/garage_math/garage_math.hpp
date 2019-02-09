19e2df779e7b826507f0f55c3002b2ff19f09842
BuildTools <qhdwight@gmail.com>
2019-02-09 12:14:56 -0800

    Added map function

--- src/garage_math/garage_math.hpp
+++ src/garage_math/garage_math.hpp
@@ -6,10 +6,15 @@
 namespace garage {
     namespace math {
         template<typename T>
-        int sign(const T& value) {
+        int sign(const T value) {
             if (value > T(0)) return 1;
             else if (value < T(0)) return -1;
             else return 0;
+        }
+
+        template<typename T>
+        double map(const T input, const T lowerInput, const T upperInput, const T lowerOutput, const T upperOutput) {
+            return lowerOutput + (upperOutput - lowerOutput) * ((input - lowerInput) / (upperInput - lowerInput));
         }
     }
 }
